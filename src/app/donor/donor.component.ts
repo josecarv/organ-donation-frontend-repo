29c19464cc3fcf1a2647dbd21b21fb0f5f6fdab4
Krishna Swathy at MHA- Contractors <swathy.krishna@contractors.gov.mt>
2024-05-28 16:02:42 +0200

    working on the mat-stepper component in the donor screen

--- src/app/donor/donor.component.ts
+++ src/app/donor/donor.component.ts
@@ -1,13 +1,20 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-donor',
   templateUrl: './donor.component.html',
-  styleUrls: ['./donor.component.css']
+  styleUrls: ['./donor.component.css'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {displayDefaultIndicatorType: false},
+    },
+  ],
 })
 export class DonorComponent implements OnInit{
-
+  isLinear =false; //if this make true, act like validation
   titleOption = 'option1';
   genderOption= 'option1';
   bloodGroupOption ='option1';
@@ -21,38 +28,25 @@ export class DonorComponent implements OnInit{
     ngOnInit(){
     this.firstFormGroup = this._formBuilder.group({
 
-    ageCtrl: ['', Validators.required],
+   
     cardNumberCtrl: ['', Validators.required],
     nameCtrl: ['', Validators.required],
     surnameCtrl: ['', Validators.required],
-    localityCtrl: ['', Validators.required],
-    postCodeCtrl: ['', Validators.required],
-    addressCtrl: ['', Validators.required],
-    professionCtrl: ['', Validators.required],
-    fatherNameCtrl: ['', Validators.required],
-    fatherSurnameCtrl: ['', Validators.required],
-    motherNameCtrl: ['', Validators.required],
-    motherSurnameCtrl: ['', Validators.required],
-    spouseNameCtrl: ['', Validators.required],
-    placeofBirthCtrl: ['', Validators.required],
-    radioFatherCtrl: ['', Validators.required],
-    radioMotherCtrl: ['', Validators.required],  
+   
+    nationalityCtrl: ['', Validators.required],
    
   });
   this.secondFormGroup = this._formBuilder.group({
     
-    placeOfDeathCtrl: ['', Validators.required],
-    causeOfDeathCtrl :  ['', Validators.required],
-    burialPermitNoCtrl : ['', Validators.required],
-    referenceNumberCtrl : ['', Validators.required],
-    massTimeCtrl : ['', Validators.required],   
-    
-    
+    emailCtrl: ['', Validators.required],
+    telephoneCtrl: ['', Validators.required],
+    mobileCtrl: ['', Validators.required],
+    contactModeCtrl: ['', Validators.required],
+   
   });
   this.thirdFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-    ownerNameCtrl: ['', Validators.required],
-    radioGraveCategoryCtrl : ['', Validators.required],  
+    addressCtrl: ['', Validators.required],
+    
   });
 }
  
