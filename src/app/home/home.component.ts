5176a8bb7c44699e161498db3693076cc51970f1
Reddy Bhavitha Reddy at MHA- Contractors <bhavitha-reddy.reddy@contractors.gov.mt>
2024-05-29 12:59:51 +0200

    intial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
-}
+  export class HomeComponent implements OnInit {
+    currentSlideIndex = 0;
+    slides = [
+      'assets/Slider_1.jpg',
+      'assets/Slider_2.jpg'
+    ];
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+      this.startSlider();
+    }
+  
+    startSlider() {
+      setInterval(() => {
+        this.nextSlide();
+      }, 3000); 
+    }
+  
+    nextSlide() {
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+    }
+  }
+  
+
+
+
+
+
+
+  
