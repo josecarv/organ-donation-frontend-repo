0cf68917a2e2a6c621e2e5d07856d17397ba136b
Reddy Bhavitha Reddy at MHA- Contractors <bhavitha-reddy.reddy@contractors.gov.mt>
2024-05-29 11:12:37 +0200

    Test push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
