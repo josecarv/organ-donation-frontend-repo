1aab24b95ba856852bec102927c3be94a519eac9
Krishna Swathy at MHA- Contractors <swathy.krishna@contractors.gov.mt>
2024-05-30 13:16:47 +0200

    working on the mat-cards in donate organ screen

--- src/app/donor/donor-organs/donor-organs.component.ts
+++ src/app/donor/donor-organs/donor-organs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-donor-organs',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./donor-organs.component.css']
 })
 export class DonorOrgansComponent {
-
+  DonorOrgansGroup!: FormGroup;
+  
+  constructor( private _formBuilder: FormBuilder,) {
+    this.DonorOrgansGroup = this._formBuilder.group({
+      donorCheckbox1Ctrl: false,
+      donorCheckbox2Ctrl: false,
+      donorCheckbox3Ctrl: false
+    });
+  }
 }
