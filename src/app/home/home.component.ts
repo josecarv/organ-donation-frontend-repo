0358f964bdd0ba8338facbceeb44aad77e893ae6
Reddy Bhavitha Reddy at MHA- Contractors <bhavitha-reddy.reddy@contractors.gov.mt>
2024-05-31 13:08:10 +0200

    Added link to website

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,29 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 
-  export class HomeComponent implements OnInit {
-    currentSlideIndex = 0;
-    slides = [
-      'assets/Slider_1.jpg',
-      'assets/Slider_2.jpg'
-    ];
-  
-    constructor() { }
-  
-    ngOnInit(): void {
-      this.startSlider();
-    }
-  
-    startSlider() {
-      setInterval(() => {
-        this.nextSlide();
-      }, 3000); 
-    }
-  
-    nextSlide() {
-      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
-    }
+  export class HomeComponent  {
   }
+   
   
 
 
