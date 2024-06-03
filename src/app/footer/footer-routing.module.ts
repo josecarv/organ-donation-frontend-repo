511f7b3aa13a355df8b2b974f1a1f9c82eff4dbc
Krishna Swathy at MHA- Contractors <swathy.krishna@contractors.gov.mt>
2024-06-03 13:28:47 +0200

    created the accessibilityscreen and made responsive

--- src/app/footer/footer-routing.module.ts
+++ src/app/footer/footer-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { PrivatePolicyComponent } from './private-policy/private-policy.component';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
 
 const routes: Routes = [
  
     { path: 'footer/disclaimer', component: DisclaimerComponent, title: 'Disclaimer' },
     { path: 'footer/copyright', component: CopyrightComponent, title: 'Copyright' },
     { path: 'footer/private-policy', component: PrivatePolicyComponent, title: 'Private-Policy' },
+    { path: 'footer/accessibility', component:AccessibilityComponent, title: 'Accessibility' },
 
    
   ];
--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -6,6 +6,7 @@ import { FooterRoutingModule } from './footer-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { PrivatePolicyComponent } from './private-policy/private-policy.component';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { PrivatePolicyComponent } from './private-policy/private-policy.componen
     DisclaimerComponent,
     CopyrightComponent,
     PrivatePolicyComponent,
+    AccessibilityComponent,
     
   ],
   imports: [
