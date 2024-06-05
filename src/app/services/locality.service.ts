12a99a159d950d3e63c32d6c2bfa174a55890f41
Krishna Swathy at MHA- Contractors <swathy.krishna@contractors.gov.mt>
2024-06-05 12:51:54 +0200

    changes made in the locality service

--- src/app/services/locality.service.ts
+++ src/app/services/locality.service.ts
@@ -1,22 +1,18 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError } from 'rxjs';
-import { ILocalityDto } from '../models/localityDto.model';
+import { ILocalityDto } from '../models/LocalityDto.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalityService {
-  private baseUrl = 'http://localhost:54213/api/locality/';
+  private baseUrl = 'http://localhost:7185/api/locality/';
   constructor(private http: HttpClient) { }
 
-  getAllLocalities(pageNumber: number = 1, pageSize: number = 100): Observable<ILocalityDto[]> {    
-    let params = new HttpParams();
-    
-    params = params.append('pageNumber', pageNumber.toString());
-    params = params.append('pageSize', pageSize.toString());
-
-    return this.http.get<ILocalityDto[]>(this.baseUrl + 'GetAllLocalities', { params })
+  getAllLocalities(): Observable<ILocalityDto[]> {   
+    return this.http.get<ILocalityDto[]>(this.baseUrl + 'GetLocalities', )
         .pipe(
             catchError(error => {
                 console.error('Error:', error);
