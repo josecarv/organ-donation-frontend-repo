02b8ea9962b060ed6b4d3b0f82e94b39363e18b5
Krishna Swathy at MHA- Contractors <swathy.krishna@contractors.gov.mt>
2024-06-24 14:57:05 +0200

    changes made in the organ cards and checkbox functionalities

--- src/app/donor/donor-organs/donor-organs.component.ts
+++ src/app/donor/donor-organs/donor-organs.component.ts
@@ -1,3 +1,5 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
@@ -5,76 +7,116 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 @Component({
   selector: 'app-donor-organs',
   templateUrl: './donor-organs.component.html',
-  styleUrls: ['./donor-organs.component.css']
+  styleUrls: ['./donor-organs.component.css'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class DonorOrgansComponent {
   DonorOrgansGroup!: FormGroup;
-  donateAllOrgans :boolean= false;
-  donateSpecificOrgans :boolean= false;
-  donateNoOrgans:boolean = false;
+  // donateAllOrgans = false;
+  // donateNoOrgans = false;
+  // selectAllOrgans = false;
+  OrgansGroup!: FormGroup;
 
-  constructor( private _formBuilder: FormBuilder,) {
+
+  flipStates :any= {
+    kidney: 'inactive',
+    bowel: 'inactive',
+    pancreas: 'inactive',
+    liver: 'inactive',
+    lungs: 'inactive',
+    heart: 'inactive',
+    cartilage: 'inactive',
+    valves: 'inactive',
+    cornea: 'inactive',
+    tissue: 'inactive',
+    ligament: 'inactive',
+    tendon: 'inactive'
+  };
+  
+  constructor(private _formBuilder: FormBuilder,){
     this.DonorOrgansGroup = this._formBuilder.group({
+      wantToBeDonor: [false],
+      donateAllOrgans: [false],
+      donateNoOrgans:[false],
+    });
+    this.OrgansGroup = this._formBuilder.group({
       kidneyStatusCtrl: [''],
-      bowelStatusCtrl: [''],
-      pancreasStatusCtrl: [''],
+      bowelStatusCtrl:[''],
+      pancreasStatusCtrl:[''],
       liverStatusCtrl: [''],
       lungsStatusCtrl: [''],
       heartStatusCtrl: [''],
       cartilageStatusCtrl: [''],
-      valvesStatusCtrl: [''],
+      valveStatusCtrl: [''],
       corneaStatusCtrl: [''],
       tissueStatusCtrl: [''],
       ligamentStatusCtrl: [''],
       tendonStatusCtrl: [''],
     });
   }
-  // onCheckboxChange(event: MatCheckboxChange) {
-  //   if (event.checked) {
-  //     this.DonorOrgansGroup.patchValue({
-  //       kidneyStatusCtrl: true,
-  //       bowelStatusCtrl: true,
-  //       pancreasStatusCtrl: true,
-  //       liverStatusCtrl: true,
-  //       lungsStatusCtrl: true,
-  //       heartStatusCtrl: true,
-  //       cartilageStatusCtrl: true,
-  //       valvesStatusCtrl: true,
-  //       corneaStatusCtrl: true,
-  //       tissueStatusCtrl: true,
-  //       ligamentStatusCtrl: true,
-  //       tendonStatusCtrl: true
-  //     });
-  //   } else {
-  //     this.DonorOrgansGroup.patchValue({
-  //       kidneyStatusCtrl: false,
-  //       bowelStatusCtrl: false,
-  //       pancreasStatusCtrl: false,
-  //       liverStatusCtrl: false,
-  //       lungsStatusCtrl: false,
-  //       heartStatusCtrl: false,
-  //       cartilageStatusCtrl: false,
-  //       valvesStatusCtrl: false,
-  //       corneaStatusCtrl: false,
-  //       tissueStatusCtrl: false,
-  //       ligamentStatusCtrl: false,
-  //       tendonStatusCtrl: false
-  //     });
-  //   }
-  // }
- 
+  
+  selectAll(event: any) {
+    Object.keys(this.OrgansGroup.controls).forEach(control => {
+      this.OrgansGroup.get(control)?.setValue(event.checked);
+    });
+  }
 
-  onCheckboxChange(type: string, event: any) {
-    if (type === 'all' && event.checked) {
-      this.donateSpecificOrgans = false;
-      this.donateNoOrgans = false;
-    } else if (type === 'specific' && event.checked) {
-      this.donateAllOrgans = false;
-      this.donateNoOrgans = false;
-    } else if (type === 'none' && event.checked) {
-      this.donateAllOrgans = false;
-      this.donateSpecificOrgans = false;
+  toggleFlip(organ: string) {
+    this.flipStates[organ] = this.flipStates[organ] === 'active' ? 'inactive' : 'active';
+  }
+  onWantToBeDonorChange(event: any) {
+    if (event.checked) {
+      this.DonorOrgansGroup.patchValue({
+        donateNoOrgans: false
+      });
+      this.DonorOrgansGroup.get('donateAllOrgans')?.setValue(true);
+      this.selectAll(event);
+    }else{
+      this.DonorOrgansGroup.get('donateNoOrgans')?.setValue(true);
+      
     }
+  }
+
+  onDonateAllOrgans(event: any) {
+    if (event.checked) {
+      this.DonorOrgansGroup.get('wantToBeDonor')?.setValue(true);
+      Object.keys(this.OrgansGroup.controls).forEach(control => {
+        this.OrgansGroup.get(control)?.setValue(true);
+      });
+      
+    }
+    else{
+      Object.keys(this.OrgansGroup.controls).forEach(control => {
+        this.OrgansGroup.get(control)?.setValue(false);
+      });
+  }
+  }
+  onDonateNoOrgans(event: any){
+    if (event.checked) {
+     
+        this.DonorOrgansGroup.patchValue({
+          wantToBeDonor: false,
+          donateAllOrgans: false
+        });
+      
+      this.DonorOrgansGroup.get('wantToBeDonor')?.setValue(false);
+      Object.keys(this.OrgansGroup.controls).forEach(control => {
+        this.OrgansGroup.get(control)?.setValue(false);
+      });
+      
+    }
+
   }
 }
 
